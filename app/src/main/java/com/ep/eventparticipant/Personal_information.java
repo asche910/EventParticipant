090877cb40260028477c77c4ab15f0135c1e8b93
Tian Wen Biao <873044564@qq.com>
2018-07-24 20:57:10 +0800

    en

---
+++ app/src/main/java/com/ep/eventparticipant/Personal_information.java
@@ -0,0 +1,13 @@
+package com.ep.eventparticipant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Personal_information extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_information);
+    }
+}
