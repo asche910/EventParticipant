e964054b2431e5d92b265ea608a720580cbaa22a
Tian Wen Biao <873044564@qq.com>
2018-07-24 21:05:44 +0800

    en

--- app/src/main/java/com/ep/eventparticipant/fragment/FragmentUser.java
+++ app/src/main/java/com/ep/eventparticipant/fragment/FragmentUser.java
@@ -1,5 +1,7 @@
 package com.ep.eventparticipant.fragment;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,19 +9,31 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.ep.eventparticipant.Personal_information;
 import com.ep.eventparticipant.R;
 
 public class FragmentUser extends Fragment {
-
+    private ImageView touxiang ;
+    private Uri imageUri;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return  inflater.from(container.getContext()).inflate(R.layout.fragment_user, container, false);
+        View view = inflater.from(container.getContext()).inflate(R.layout.fragment_user, container, false);
+        touxiang = (ImageView)view.findViewById(R.id.touxiang);
+        return  view;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        touxiang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), Personal_information.class);
+                startActivity(intent);
+            }
+        });
     }
 }
